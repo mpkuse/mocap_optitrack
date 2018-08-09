ceea545f3b620626b3fb7eea792e59a581de5e60
Manohar Kuse <mpkuse@connect.ust.hk>
2018-08-09 11:55:35 +0800

    bug in parent frame id. setting frame_id as world

--- src/mocap_config.cpp
+++ src/mocap_config.cpp
@@ -8,16 +8,16 @@
  *   \ \_/ \_/ /  | |  | |  | ++ | |_| || ++ / | ++_/| |_| |  | |  | +-+ |
  *    \  \_/  /   | |_ | |_ | ++ |  _  || |\ \ | |   |  _  |  | |  | +-+ |
  *     \_____/    \___/|___||___||_| |_||_| \_\|_|   |_| |_|  |_|  |_| |_|
- *             ROBOTICS™ 
+ *             ROBOTICS™
  *
  *  File: mocap_config.cpp
  *  Desc: Classes representing ROS configuration for mocap_optitrack node. Data
  *  will be published to differed topics based on the configuration provided.
  *  Auth: Alex Bencz
  *
- *  Copyright (c) 2012, Clearpath Robotics, Inc. 
+ *  Copyright (c) 2012, Clearpath Robotics, Inc.
  *  All Rights Reserved
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -28,7 +28,7 @@
  *     * Neither the name of Clearpath Robotics, Inc. nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -39,8 +39,8 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * Please send comments, questions, or patches to skynet@clearpathrobotics.com 
+ *
+ * Please send comments, questions, or patches to skynet@clearpathrobotics.com
  *
  */
 #include <geometry_msgs/PoseStamped.h>
@@ -59,7 +59,7 @@ PublishedRigidBody::PublishedRigidBody(XmlRpc::XmlRpcValue &config_node)
   publish_pose = validateParam(config_node, POSE_TOPIC_PARAM_NAME);
   publish_pose2d = validateParam(config_node, POSE2D_TOPIC_PARAM_NAME);
   // only publish tf if a frame ID is provided
-  publish_tf = (validateParam(config_node, CHILD_FRAME_ID_PARAM_NAME) && 
+  publish_tf = (validateParam(config_node, CHILD_FRAME_ID_PARAM_NAME) &&
                validateParam(config_node, PARENT_FRAME_ID_PARAM_NAME));
 
   if (publish_pose)
@@ -83,6 +83,8 @@ PublishedRigidBody::PublishedRigidBody(XmlRpc::XmlRpcValue &config_node)
 
 void PublishedRigidBody::publish(RigidBody &body)
 {
+  parent_frame_id = "world"; 
+  cout << "PublishedRigidBody::publish : " << parent_frame_id << "\n";
   // don't do anything if no new data was provided
   if (!body.has_data())
   {
@@ -153,4 +155,3 @@ bool PublishedRigidBody::validateParam(XmlRpc::XmlRpcValue &config_node, const s
 
   return true;
 }
-
